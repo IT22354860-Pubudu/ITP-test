ae4d5b0b5a9cd52bff1e4ff72c2b3573160dbe3c
IT22354860-Pubudu <it22354860@my.sliit.lk>
2024-03-05 15:31:40 +0530

    hii

--- event-management/src/App.js
+++ event-management/src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Pubudu darshana</h1>
+        <h4>Event manegemant</h4>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
