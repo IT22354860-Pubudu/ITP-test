3982197e8e4d5e6d903f050a48e72d6c349080d9
SanujaMalinntha <snujamalintharandeni@gail.com>
2024-03-06 23:39:47 +0530

    ji

--- event-management/src/App.js
+++ event-management/src/App.js
@@ -1,13 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import Singup from './pages/singup'; 
-import Card from './component/card';
 
 function App() {
   return (
-    <div>
-        <Singup/>
-        <Card name='pubudu'/>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
